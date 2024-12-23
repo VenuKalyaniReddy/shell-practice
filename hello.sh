65cda7ce6b4665b0be015cd15f885f1cb78edb42
VenuKalyaniReddy <devopsengineer11111@gmail.com>
2024-12-23 16:05:54 +0530

    first commit

---
+++ hello.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+#This is comment
+echo "hello Shell-script"
---
+++ variable.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+PERSON1 = RAMESH
+PERSON2 = SURESH
+
+echo "$PERSON1: Hello $PERSON2 , Good Mrning"
+echo "$PERSON2: hello $PERSON1, Very Good Morning"
+echo "$PERSON1: How are you $PERSON2?"
+echo "$PERSON2: I am fine How are you $PERSON1"
+
