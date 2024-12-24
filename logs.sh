87ccac61b53e245258a61419cabe19f2ad752cc7
VenuKalyaniReddy <devopsengineer11111@gmail.com>
2024-12-24 15:22:02 +0530

    Commits of Shell

--- logs.sh
+++ logs.sh
@@ -9,17 +9,17 @@ LOGFILE="/tmp/$0-$TIMESTAMP.log"
 VALIDTE() {
     if [ $1 -ne 0 ]
     then 
-        echo "ERROR:: $2 ...FAILED"
+        echo "ERROR:: $2 ...FAILED" "$LOGFILE"
         exit 1
     else
-        echo "ALL Good $2....SUCCESS"
+        echo "ALL Good $2....SUCCESS" "$LOGFILE"
     fi
 }
 
 if [ $ID -ne 0 ]
 then 
-    echo "ERROR:: Please run this script with root acess"
+    echo "ERROR:: Please run this script with root acess" "$LOGFILE"
     exit 1
 else
-    echo "ALL Good you are root user"
+    echo "ALL Good you are root user" "$LOGFILE"
 fi
