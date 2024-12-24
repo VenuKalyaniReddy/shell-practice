925e10f63ec7ccbbb93d56bb0d5301b575d96776
VenuKalyaniReddy <devopsengineer11111@gmail.com>
2024-12-24 14:19:40 +0530

    Commits of Shell

---
+++ Arguments.sh
@@ -0,0 +1,26 @@
+###special arguments
+## > =Redirection
+## >> =Append
+## 1 =success
+## 2 =Failure
+## & =Both sucess and Failure
+
+#Special Arguments
+# $? = exit status of previous command
+# $0 = you will get script name
+# $1 = 1st argument
+# $2 = 2nd argument
+# $@ = for all argument
+# $# = Total no.of arguments
+
+
+#! /bin/bash
+ NUMBER1 =$1
+ NUMBNER2 =$2
+ SUM = $(( $NUMBER1 + $NUMBER2 ))
+ echo "Total:: $SUM"
+ echo "How many args passed:: $#"
+ echo "All args passed:: $@"
+ echo "Script name:: $0"
+
+
---
+++ logs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+ID =$(id -u)
+
+TIMESTAMP =$(date +%F-%H-%M-%S)
+LOGFILE= "/tmp/$0-$TIMESTAMP.log"
+
+VALIDTE() {
+    if [ $1 -ne 0 ]
+    then 
+        echo "ERROR:: $2 ...FAILED"
+        exit1
+    else
+        echo "ALL Good $2....SUCCESS"
+    fi
+}
+
+if [$ID -ne 0]
+then 
+    echo "ERROR:: Please run this script with root acess"
+    exit1
+else
+    echo "ALL Good you are root user"
+fi
